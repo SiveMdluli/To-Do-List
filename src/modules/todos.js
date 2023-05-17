5a2125a0c17723b645e765f5304b24b357988eee
Sive Mdluli <mdlulisive202@gmail.com>
2023-05-17 15:14:57 +0200

    Add modules, which work hand in had with entry point file[index.js]

---
+++ src/modules/todos.js
@@ -0,0 +1,52 @@
+import _ from "lodash";
+import { clearCompletedButton } from './ui.js';
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+const todoList = document.getElementById('todo-list');
+
+const renderTodoList = () => {
+  todoList.innerHTML = '';
+
+  todos.forEach(todo => {
+    const taskListItem = document.createElement('li');
+    taskListItem.classList.add('task-list-item');
+
+    const checkboxWrapper = document.createElement('label');
+    checkboxWrapper.classList.add('checkbox-wrapper');
+
+    const checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox');
+
+    checkbox.type = 'checkbox';
+    checkbox.checked = todo.completed;
+
+    const checkmark = document.createElement('span');
+    checkmark.classList.add('checkmark');
+
+    checkboxWrapper.appendChild(checkbox);
+    checkboxWrapper.appendChild(checkmark);
+
+    const taskDescription = document.createElement('span');
+    taskDescription.classList.add('task-description');
+    taskDescription.textContent = todo.text;
+    if (todo.completed) {
+      taskDescription.classList.add('completed');
+    }
+    const icon = document.createElement('span');
+    icon.classList.add('fas', 'fa-ellipsis-v');
+    icon.style.color = '#c8ccd0';
+
+    taskListItem.appendChild(checkboxWrapper);
+    taskListItem.appendChild(taskDescription);
+    taskListItem.appendChild(icon);
+    todoList.appendChild(taskListItem);
+  });
+
+//   clearCompletedButton.style.display = todos.some(todo => todo.completed) ? 'block' : 'none';
+  clearCompletedButton.style.display = 'block';
+
+  // Store the updated todos array in localStorage
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+export { todos, renderTodoList, todoList };
---
+++ src/modules/ui-actions.js
@@ -0,0 +1,25 @@
+import _ from "lodash";
+const addItem = (todos, input, render) => {
+    const newItemText = input.value.trim();
+  
+    if (newItemText === '') {
+      return;
+    }
+  
+    const newTodo = {
+      id: todos.length + 1,
+      text: newItemText,
+      completed: false
+    };
+  
+    todos.push(newTodo);
+    input.value = '';
+    render();
+  };
+  
+  const clearCompleted = (todos, render) => {
+    todos = todos.filter(todo => !todo.completed);
+    render();
+  };
+  
+  export { addItem, clearCompleted };
