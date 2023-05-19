65b12b47b6689fea1dd9ad55326d39620f69a303
Sive Mdluli <mdlulisive202@gmail.com>
2023-05-19 16:44:17 +0200

    Add the add and remove functionality

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,57 +1,58 @@
-// import _ from 'lodash';
-import { clearCompletedButton } from './ui.js';
+import { addItem, clearCompleted } from "./ui-actions.js";
 
-const todos = [
-  {
-    description: 'Create a dance style to scynchonously jam to the sound of a washing machine.',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Program a script that can translate thoughts into code.',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Create an app that generates random programming pickup lines.',
-    completed: false,
-    index: 2,
-  },
-];
-
-const todoList = document.getElementById('todo-list');
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+const todoList = document.getElementById("todo-list");
+const input = document.getElementById("new-item-input");
+const clearCompletedButton = document.getElementById("clear-completed-button");
+const addItemButton = document.getElementById("add-item-button");
 
 const renderTodoList = () => {
-  todoList.innerHTML = '';
+  todoList.innerHTML = "";
 
-  todos.forEach((todo) => {
-    const todosListItem = document.createElement('li');
-    todosListItem.classList.add('task-list-item');
+  const updatedTodos = clearCompleted(todos);
+  todos = updatedTodos;
 
-    const checkboxWrapper = document.createElement('label');
-    checkboxWrapper.classList.add('checkbox-wrapper');
+  updatedTodos.forEach((todo) => {
+    const todosListItem = document.createElement("li");
+    todosListItem.classList.add("task-list-item");
 
-    const checkbox = document.createElement('input');
-    checkbox.classList.add('checkbox');
+    const checkboxWrapper = document.createElement("label");
+    checkboxWrapper.classList.add("checkbox-wrapper");
 
-    checkbox.type = 'checkbox';
+    const checkbox = document.createElement("input");
+    checkbox.classList.add("checkbox");
+
+    checkbox.type = "checkbox";
     checkbox.checked = todo.completed;
 
-    const checkmark = document.createElement('span');
-    checkmark.classList.add('checkmark');
+    const checkmark = document.createElement("span");
+    checkmark.classList.add("checkmark");
 
     checkboxWrapper.appendChild(checkbox);
     checkboxWrapper.appendChild(checkmark);
 
-    const taskDescription = document.createElement('span');
-    taskDescription.classList.add('task-description');
+    const taskDescription = document.createElement("span");
+    taskDescription.classList.add("task-description");
     taskDescription.textContent = todo.description;
+
     if (todo.completed) {
-      taskDescription.classList.add('completed');
+      taskDescription.classList.add("completed");
     }
-    const icon = document.createElement('span');
-    icon.classList.add('fas', 'fa-ellipsis-v');
-    icon.style.color = '#c8ccd0';
+
+    const icon = document.createElement("span");
+    icon.classList.add("fas", "fa-ellipsis-v");
+    icon.style.color = "#c8ccd0";
+
+    checkbox.addEventListener("change", () => {
+      todo.completed = checkbox.checked;
+      localStorage.setItem("todos", JSON.stringify(todos));
+      const updatedTodos = clearCompleted(todos);
+      if (updatedTodos.some((todo) => todo.completed)) {
+        clearCompletedButton.style.display = "none";
+      } else {
+        clearCompletedButton.style.display = "block";
+      }
+    });
 
     todosListItem.appendChild(checkboxWrapper);
     todosListItem.appendChild(taskDescription);
@@ -59,9 +60,21 @@ const renderTodoList = () => {
     todoList.appendChild(todosListItem);
   });
 
-  clearCompletedButton.style.display = 'block';
+  clearCompletedButton.style.display = updatedTodos.some(
+    (todo) => todo.completed
+  )
+    ? "none"
+    : "block";
+  localStorage.setItem("todos", JSON.stringify(updatedTodos));
 
-  localStorage.setItem('todos', JSON.stringify(todos));
+  addItemButton.addEventListener("click", () => {
+    addItem(todos, input, renderTodoList);
+  });
+
+  clearCompletedButton.addEventListener("click", () => {
+    todos = clearCompleted(todos);
+    renderTodoList();
+  });
 };
 
 export { todos, renderTodoList, todoList };
