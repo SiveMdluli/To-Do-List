450fc5a4f7e89d48f5a557d512c250b385c1dce1
Sive Mdluli <mdlulisive202@gmail.com>
2023-05-20 11:40:34 +0200

    Add remove functionality and edit, on an individual item

--- src/modules/todos.js
+++ src/modules/todos.js
@@ -43,6 +43,74 @@ const renderTodoList = () => {
     icon.classList.add('fas', 'fa-ellipsis-v');
     icon.style.color = '#c8ccd0';
 
+    // Add an event lisnter to the icon
+    icon.addEventListener('click', (e) => {
+      showOptionsMenu(e);
+    });
+
+    function showOptionsMenu(e) {
+      const optionsMenus = document.querySelectorAll('.options-menu');
+      // Hide all other options menus
+      optionsMenus.forEach((menu) => {
+        if (menu !== e.target.querySelector('.options-menu')) {
+          menu.remove();
+        }
+      });
+
+      const optionsMenu = document.createElement('div');
+      optionsMenu.classList.add('options-menu');
+
+      const editButton = document.createElement('button');
+      editButton.textContent = 'Edit';
+      editButton.addEventListener('click', () => {
+        editTodo(todo);
+        hideOptionsMenu();
+      });
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Delete';
+      deleteButton.addEventListener('click', () => {
+        deleteTodo(todo);
+        hideOptionsMenu();
+      });
+
+      optionsMenu.appendChild(editButton);
+      optionsMenu.appendChild(deleteButton);
+      e.target.appendChild(optionsMenu);
+    }
+
+    function hideOptionsMenu() {
+      const optionsMenu = document.querySelector('.options-menu');
+      if (optionsMenu !== null) {
+        optionsMenu.remove();
+      }
+    }
+
+    function editTodo(todo) {
+      const todoIndex = todo.index;
+      input.value = todo.description;
+      addItemButton.innerText = 'Save';
+
+      const saveItem = () => {
+        todos[todoIndex].description = input.value;
+        localStorage.setItem('todos', JSON.stringify(todos));
+        renderTodoList();
+        hideOptionsMenu();
+        addItemButton.innerText = '+';
+        addItemButton.classList.add('plus-on-edit');
+        addItemButton.removeEventListener('click', saveItem);
+      };
+
+      addItemButton.addEventListener('click', saveItem);
+    }
+
+    const deleteTodo = (todo, e) => {
+      const todoIndex = todos.indexOf(todo);
+      todos.splice(todoIndex, 1);
+      localStorage.setItem('todos', JSON.stringify(todos));
+      renderTodoList();
+    };
+
     checkbox.addEventListener('change', () => {
       todo.completed = checkbox.checked;
       localStorage.setItem('todos', JSON.stringify(todos));
@@ -56,11 +124,7 @@ const renderTodoList = () => {
     todoList.appendChild(todosListItem);
   });
 
-  clearCompletedButton.style.display = updatedTodos.some(
-    (todo) => todo.completed,
-  )
-    ? 'none'
-    : 'block';
+  clearCompletedButton.style.display = updatedTodos.some((todo) => todo.completed) ? 'none' : 'block';
   localStorage.setItem('todos', JSON.stringify(updatedTodos));
 
   addItemButton.addEventListener('click', () => {
